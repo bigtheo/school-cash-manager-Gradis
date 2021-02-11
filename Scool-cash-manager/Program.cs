48792b2b1125771b802bdc62109776a2b5b1c995
Théophile KAPAPA <53141608+bigtheo@users.noreply.github.com>
2021-02-11 18:24:17 +0200

    ajout du fichier solution

--- Scool-cash-manager/Program.cs
+++ Scool-cash-manager/Program.cs
@@ -1,22 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Scool_cash_manager
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// Point d'entrée principal de l'application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmMenuPrincipal());
         }
     }
 }
